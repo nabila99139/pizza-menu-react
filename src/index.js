aee4a54af1f088341ff458bce77e6613d7a3b568
Nabila Hassya <nabilahassya99@gmail.com>
2026-04-26 19:59:59 +0700

    condition and map

--- src/index.js
+++ src/index.js
@@ -71,26 +71,41 @@ function Header() {
 
 function Menu() {
   // const pizzas = [];
-  const pizzas = pizzaData
+  const pizzas = pizzaData;
   const numPizzas = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our Menu :</h2>
-      {numPizzas > 0 && (
+      {/* {numPizzas > 0 && (
         <ul className="pizzas">
-          {/* {pizzaData.map((pizza) => (
-          <Pizza
-            name={pizza.name}
-            ingredients={pizza.ingredients}
-            photoName={pizza.photoName}
-            price={pizza.price}
-          />
-        ))} */}
           {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+            <Pizza
+              name={pizza.name}
+              ingredients={pizza.ingredients}
+              photoName={pizza.photoName}
+              price={pizza.price}
+            />
+            ))}
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+              ))}
+              </ul>
+              )} */}
+      {numPizzas > 0 ? (
+        <> {/*React.fragment key="" -> ini kependekan dari react fragment*/}
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>we're still working on our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -112,28 +127,53 @@ function Footer() {
   //   alert("Sorry, we're closed")
   // }
 
+  if (!isOpen) return <p>CLOSED</p>;
+
   return (
     <footer className="footer">
       {/* {new Date().toLocaleTimeString()}. We're currently open! */}
-      <div className="order">
+      {/* <div className="order">
         {isOpen && (
           <p>We're open until {closeHour}:00. Come visit us or order online</p>
         )}
         <button className="btn">Order</button>
-      </div>
+      </div> */}
+      {isOpen ? (
+        <Order closeHour={closeHour} isOpen={isOpen} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
     </footer>
   );
 }
 
-function Pizza(props) {
-  // console.log(props);
+function Order(props) {
   return (
-    <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    <div className="order">
+      {props.isOpen && (
+        <p>
+          We're open until {props.closeHour}:00. Come visit us or order online
+        </p>
+      )}
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+
+function Pizza({ pizzaObj }) {
+  // console.log(props);
+
+  // if (pizzaObj.soldOut) return null;
+
+  return (
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? 'SOLD OUT!' : pizzaObj.price + 3}</span>
       </div>
     </li>
   );
